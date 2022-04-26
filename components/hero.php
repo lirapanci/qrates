6f28b46a920841dbab407798df1f3b46619adb72
Lira Panci <pancilira@gmail.com>
2022-04-26 22:17:51 +0200

    setting up flexible content

---
+++ components/hero.php
@@ -0,0 +1,11 @@
+<div>
+    <?php
+
+    echo get_sub_field('title');
+    echo get_sub_field('text');
+    echo get_sub_field('main_image');
+    echo get_sub_field('secondary_image');
+    echo get_sub_field('logo');
+
+    ?>
+</div>
--- index.php
+++ index.php
@@ -2,5 +2,14 @@
     exit;
 } ?>
 
+<?php get_header();
 
-<?php get_header(); ?>
+if (have_rows('content')):
+
+    while (have_rows('content')) : the_row();
+        include(get_template_directory() . '/components/' . get_row_layout() . '.php');
+    endwhile;
+
+else :
+    echo 'NO CONTENT';
+endif;
